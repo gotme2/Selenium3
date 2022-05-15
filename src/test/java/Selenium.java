c4756698342957c00f7e89cbde4f9bc89c0c9fef
PLUGG <bellange@kth.se>
2022-05-15 18:53:41 +0200

    .

--- src/test/java/Selenium.java
+++ src/test/java/Selenium.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Selenium {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium {
+    public static void main(String[] args){
+        ChromeDriver driver=new ChromeDriver();
+        driver.get("http://demo.guru99.com/");
+        WebElement element=driver.findElement(By.xpath("//body/form[@name='frmLogin']/table//input[@name='emailid']"));
+        element.sendKeys("leobel-1@student.ltu.se");
+
+        WebElement button=driver.findElement(By.xpath("//body/form[@name='frmLogin']/table//input[@name='emailid']"));
+        button.click();
+
+    }
 }
